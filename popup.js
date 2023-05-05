ca56ebb3f2046cff56f57dbba0cfd5fa25af535e
Zoneam <zoneam@gmail.com>
2023-05-05 01:06:08 -0700

    minor changes in structure

--- popup.js
+++ popup.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Popup script running');
+    console.log('Popup script');
   });
--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 
 window.addEventListener('load', function() {
     const dpDiv = document.getElementById('dp');
-        console.log('Content script running',dpDiv)
     if (dpDiv) {
       const reviewDiv = document.createElement('div');
       reviewDiv.setAttribute('id', 'review');
-      reviewDiv.innerHTML = '⭐⭐⭐⭐⭐ This is the best product in the market! ⭐⭐⭐⭐⭐';
+      reviewDiv.innerHTML = '⭐⭐⭐⭐⭐ This is the best product on the market! ⭐⭐⭐⭐⭐';
       dpDiv.parentNode.insertBefore(reviewDiv, dpDiv);
     }
   });
