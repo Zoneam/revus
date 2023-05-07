19825d07f7f97be234b50f9808dc9a6ea8af584f
Zoneam <zoneam@gmail.com>
2023-05-07 14:19:28 -0700

    extracting top reviews from the product page

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-
-
 window.addEventListener('load', function() {
-    const dpDiv = document.getElementById('dp');
-    if (dpDiv) {
-      const reviewDiv = document.createElement('div');
-      reviewDiv.setAttribute('id', 'review');
-      reviewDiv.innerHTML = '⭐⭐⭐⭐⭐ This is the best product on the market! ⭐⭐⭐⭐⭐';
-      dpDiv.parentNode.insertBefore(reviewDiv, dpDiv);
-    }
+  const allReviewTexts = [];
+  const dpDiv = document.getElementById('dp');
+  if (dpDiv) {
+    const reviewDiv = document.createElement('div');
+    reviewDiv.setAttribute('id', 'review');
+    reviewDiv.innerHTML = '⭐⭐⭐⭐⭐ This is the best product on the market! ⭐⭐⭐⭐⭐';
+    dpDiv.parentNode.insertBefore(reviewDiv, dpDiv);
+  }
+
+  const reviewSpans = document.querySelectorAll('.review-text-content > span');
+
+  reviewSpans.forEach((span) => {
+    const review = span.textContent.trim();
+    reviewDataSet.push(review);
   });
+
+  console.log(reviewDataSet);
+
+  });
+  
+
   
