a085d6266edb52306dad80223dc523d023a717ec
Zoneam <zoneam@gmail.com>
2023-05-07 17:57:00 -0700

    plugging in openAI api, adding webpack

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-  window.addEventListener('load', async function() {
-    const reviewDataSet = [];
+window.addEventListener("load", async function () {
+  const reviewDataSet = [];
 
-    const dpDiv = document.getElementById('dp');
-    if (dpDiv) {
-      const reviewDiv = document.createElement('div');
-      reviewDiv.setAttribute('id', 'review');
-      reviewDiv.innerHTML = '⭐⭐⭐⭐⭐ This is the best product on the market! ⭐⭐⭐⭐⭐';
-      dpDiv.parentNode.insertBefore(reviewDiv, dpDiv);
-    }
+  const dpDiv = document.getElementById("dp");
+  if (dpDiv) {
+    const reviewDiv = document.createElement("div");
+    reviewDiv.setAttribute("id", "review");
+    reviewDiv.innerHTML =
+      "⭐⭐⭐⭐⭐ This is the best product on the market! ⭐⭐⭐⭐⭐";
+    dpDiv.parentNode.insertBefore(reviewDiv, dpDiv);
+  }
 
-    // Get all the reviews
-    const reviewSpans = document.querySelectorAll('.review-text-content > span');
+  // Get all the reviews
+  const reviewSpans = document.querySelectorAll(".review-text-content > span");
 
-    reviewSpans.forEach((span) => {
-      const review = span.textContent.trim();
-      reviewDataSet.push(review);
-    });
-
-    console.log(reviewDataSet);
-
-    // Analize the reviewDataSet with AI
-    
-    const summary = await summarizeReviews(reviewDataSet)
-
-    // Display the results
-
-    console.log(summary);
-
+  reviewSpans.forEach((span,idx) => {
+    const review = span.textContent.trim();
+    reviewDataSet.push("Customer " + idx + ": " + review);
   });
-  
 
-  async function summarizeReviews(reviews) {
-    const url = 'https://api.openai.com/v1/models/text-davinci-002/generate';
+  console.log(reviewDataSet);
+
+  // Summarize the reviewDataSet with AI
+
+  const summary = await summarizeReviews(reviewDataSet);
+
+  // Display the results
+
+
+});
+
+async function summarizeReviews(reviews) {
+  try {
+    const allReviews = reviews.join("\n\n");
+    const myApiKey = process.env.API_KEY;
+    const url = "https://api.openai.com/v1/completions";
     const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_KEY}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${myApiKey}`,
     };
-    const data = {
-      prompt: `Summarize:\n\n${reviews}\n\nSummary:`,
+    const dataRec = {
+      prompt: `give bullet point summary of recurring problems with this product:\n\n${allReviews}\n\n`,
       temperature: 0.7,
-      max_tokens: 100,
+      model: "text-davinci-002",
+      max_tokens: 250,
       top_p: 1,
       frequency_penalty: 0,
-      presence_penalty: 0
+      presence_penalty: 0,
     };
-    try {
-      const response = await axios.post(url, data, { headers });
-      return response.data
-    } catch (error) {
-      console.error(error);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(dataRec)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const data = await response.json();
+    console.log('Data====',data.choices[0].text);
+    return data.choices[0].text;
+  } catch (error) {
+    console.error('ERROR====',error);
   }
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DotenvPlugin = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    background: './background.js',
+    script: './script.js',
+    popup: './popup.js',
+    style: './style.css'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './popup.html',
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+    new DotenvPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'manifest.json', to: 'manifest.json' },
+        { from: 'icon48.png', to: 'icon48.png' }
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+};
