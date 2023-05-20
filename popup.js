90dd8e6f4a45b08c6ac42f09620ed7cae4d1552f
Zoneam <zoneam@gmail.com>
2023-05-20 01:35:22 -0700

    adding activation by button click

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Popup script');
+document.getElementById('activateButton').addEventListener('click', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "summarize"});
   });
+});
--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-window.addEventListener("load", async function () {
-  let reviewDataSet = '';
-
-  const dpDiv = document.getElementById("dp");
-  const imageDiv = document.getElementById("imageBlock");
-
-  // Get all the reviews
-  const reviewSpans = document.querySelectorAll(".review-text-content > span");
-
-  reviewSpans.forEach((span, idx) => {
-    const review = span.textContent.trim();
-    reviewDataSet += `Review ${idx + 1}: ${review}\n`;
-  });
-
-  // Summarize the reviewDataSet with AI
-
-  const summary = await summarizeReviews({ reviews: reviewDataSet });
-  
-  // Display the results
-
-  if (imageDiv) {
-    const reviewDiv = document.createElement("div");
-    reviewDiv.setAttribute("id", "review");
-
-    if (summary.length){
-      reviewDiv.innerHTML += `<li class="list-problem">⭐ ${summary}</li>`;
+chrome.runtime.onMessage.addListener(
+  async function(request, sender, sendResponse) {
+    if (request.greeting === "summarize") {
+        let reviewDataSet = '';
+        const imageDiv = document.getElementById("imageBlock");
+      
+        // Get all the reviews
+        const reviewSpans = document.querySelectorAll(".review-text-content > span");
+      
+        reviewSpans.forEach((span, idx) => {
+          const review = span.textContent.trim();
+          reviewDataSet += `Review ${idx + 1}: ${review}\n`;
+        });
+      
+        // Summarize the reviewDataSet with AI
+        const summary = await summarizeReviews({ reviews: reviewDataSet });
+        
+        // Add result to the DOM under image
+        if (imageDiv) {
+          const reviewDiv = document.createElement("div");
+          reviewDiv.setAttribute("id", "review");
+      
+          if (summary.length){
+            reviewDiv.innerHTML += `<li class="list-problem">⭐ ${summary}</li>`;
+          }
+          imageDiv.appendChild(reviewDiv);
+        }
     }
-
-    imageDiv.appendChild(reviewDiv);
-  }
 });
 
-async function summarizeReviews(reviews) {
+// Summarize reviews using the Revus Lambda on AWS
+async function summarizeReviews(reviews, maxRetries = 3) {
+  let reviewsString;
   const url = 'https://z6xdsaipm1.execute-api.us-east-1.amazonaws.com/dev/revus';
   const headers = {
     'Content-Type': 'application/json',
   };
 
-  if (reviews.length !== 0) {
+  if (reviews.length === 0) {
+    console.error('No reviews provided');
+    return;
+  }
+
+  try {
+    reviewsString = JSON.stringify(reviews);
+  } catch (error) {
+    console.error('Failed to stringify reviews', error);
+    return {
+      error: true,
+      message: `Failed to stringify reviews. Error: ${error.message}`
+    };
+  }
+
+  for (let i = 0; i <= maxRetries; i++) {
     try {
       const response = await fetch(url, {
         method: 'POST',
         headers: headers,
-        body: JSON.stringify(reviews)
+        body: reviewsString
       });
+
       if (response.ok) {
         return response.json();
-      } else if (response.status >= 500 && response.status < 600) {
-        console.error('Server error. Status code:', response.status);
       } else {
-        console.error('Error. Status code:', response.status);
+        throw new Error(`Response error with status code: ${response.status}`);
       }
     } catch (error) {
-      console.error('ERROR====', error);
+      console.error('Attempt', i + 1, 'ERROR====', error);
+
+      // Only retry for 5xx server errors
+      if (i < maxRetries && error.message.startsWith('Response error') && parseInt(error.message.split(': ')[1]) >= 500) {
+        continue;
+      } else {
+        // Return or throw the error if the maximum number of retries has been reached, or for non-5xx errors
+        return {
+          error: true,
+          message: `Failed to summarize reviews after ${i + 1} attempts. Error: ${error.message}`
+        };
+      }
     }
   }
 }
