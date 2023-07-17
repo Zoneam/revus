82f0fa47a204a0d68c8884485d7b1a09f33f8af8
Zoneam <zoneam@gmail.com>
2023-07-16 19:49:30 -0700

    Restructuring so button is on the page rather than in extension page

--- popup.js
+++ popup.js
@@ -1,24 +1,24 @@
 
-console.log('popup.js loaded');
+// console.log('popup.js loaded');
 
-import './popup.css';
-let getSummaryBtn = document.getElementById('getSummaryBtn');
-// When the popup is opened, check the stored state
-chrome.storage.local.get('targetVisible', (result) => {
-  if (result.targetVisible) {
-      // The target is visible, activate the button
-      getSummaryBtn.innerText = 'Summarize Reviews';
-  } else {
-      getSummaryBtn.innerText = 'No Reviews Found'; 
-  }
-});
+// import './popup.css';
+// let getSummaryBtn = document.getElementById('getSummaryBtn');
+// // When the popup is opened, check the stored state
+// chrome.storage.local.get('targetVisible', (result) => {
+//   if (result.targetVisible) {
+//       // The target is visible, activate the button
+//       getSummaryBtn.innerText = 'Summarize Reviews';
+//   } else {
+//       getSummaryBtn.innerText = 'No Reviews Found'; 
+//   }
+// });
 
 
-if (!getSummaryBtn.hasListener) {
-  getSummaryBtn.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "summarize"});
-    });
-  });
-  getSummaryBtn.hasListener = true;
-}
+// if (!getSummaryBtn.hasListener) {
+//   getSummaryBtn.addEventListener('click', function() {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.sendMessage(tabs[0].id, {message: "summarize"});
+//     });
+//   });
+//   getSummaryBtn.hasListener = true;
+// }
